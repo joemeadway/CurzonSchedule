58452032dbdfdcde2b366561f5cf05c9634cd338
Joe Meadway <joemeadway@gmail.com>
2018-04-11 20:13:58 +0100

    added tests for getting cinema name

---
+++ test/CurzonSchedule.Test/SiteParser/GetCinemaNameShould.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using sut = CurzonSchedule;
+
+namespace CurzonSchedule.Test.SiteParser
+{
+    public class GetCinemaNameShould
+    {
+        [Fact]
+        public void ReturnName_WhenPageContainsIt()
+        {
+            var parser = new sut.SiteParser();
+
+            var parentDoc = new HtmlDocument();
+            var body = new HtmlNode(HtmlNodeType.Element, parentDoc, 0);
+            body.Name = "body";
+            body.Attributes.Add("data-cinema-name", "Cinema Name");
+            parentDoc.DocumentNode.AppendChild(body);
+
+            var result = parser.GetCinemaName(parentDoc);
+
+            Assert.Equal("Cinema Name", result);
+        }
+
+        [Fact]
+        public void ReturnEmptyString_WhenPageDoesNotHaveCinemaNameAttribute()
+        {
+            var parser = new sut.SiteParser();
+
+            var parentDoc = new HtmlDocument();
+            var body = new HtmlNode(HtmlNodeType.Element, parentDoc, 0);
+            body.Name = "body";
+            body.Attributes.Add("data-cinema-name", "");
+            parentDoc.DocumentNode.AppendChild(body);
+
+            var result = parser.GetCinemaName(parentDoc);
+
+            Assert.Equal("", result);
+        }
+    }
+}
