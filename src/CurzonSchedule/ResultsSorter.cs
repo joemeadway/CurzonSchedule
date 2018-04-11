f22661c373b85a83313ac98880b52abaaba1ee9d
Joe Meadway <joemeadway@gmail.com>
2018-04-11 21:57:21 +0100

    added sort order options

---
+++ src/CurzonSchedule/ResultsSorter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CurzonSchedule
+{
+    public static class ResultsSorter
+    {
+        public static IEnumerable<Showing> SortThisBy(this IEnumerable<Showing> input, SortOrder sortBy)
+        {
+            if (input.Count() == 0 || input.Count() == 1)
+                return input;
+            switch (sortBy)
+            {
+                case SortOrder.Cinema:
+                    input = SortByCinema(input);
+                    return input;
+                case SortOrder.Date:
+                    input = SortByDate(input);
+                    return input;
+                case SortOrder.Film:
+                    input = SortByFilm(input);
+                    return input;
+                default:
+                    break;
+            }
+            return input;
+        }
+
+        private static IEnumerable<Showing> SortByFilm(IEnumerable<Showing> input)
+        {
+            input = input.OrderBy(s => s, new ShowingFilmComparer()).ToList();
+            return input;
+        }
+
+        private static IEnumerable<Showing> SortByDate(IEnumerable<Showing> input)
+        {
+            input = input.OrderBy(s => s.When).ToList();
+            return input;
+        }
+
+        private static IEnumerable<Showing> SortByCinema(IEnumerable<Showing> input)
+        {
+            input = input.OrderBy(s => s, new ShowingCinemaComparer()).ToList();
+            return input;
+        }
+    }
+}
---
+++ src/CurzonSchedule/SortOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurzonSchedule
+{
+    public enum SortOrder
+    {
+        Cinema = 0,
+        Date = 1,
+        Film = 2
+    }
+}
